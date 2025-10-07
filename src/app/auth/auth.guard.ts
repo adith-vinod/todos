885e0852787608aa4fc156d9db5e30853ddc2138
advin <adithpsycho@gmail.com>
2025-10-07 16:16:33 +0530

    refresh bug fix

--- src/app/auth/auth.guard.ts
+++ src/app/auth/auth.guard.ts
@@ -1,16 +1,18 @@
 import { inject } from '@angular/core';
-import { Auth } from '@angular/fire/auth';
+import { Auth, authState } from '@angular/fire/auth';
 import { CanMatchFn, Router } from '@angular/router';
+import { map, take } from 'rxjs';
 
 export const authGuard: CanMatchFn = (route, segments) => {
   const auth = inject(Auth);
   const router = inject(Router);
-  const user = auth.currentUser;
-
-  if(user){
-    return true
-  }else{
-    router.navigate(['/'])
-    return false
-  }
+  
+  return authState(auth).pipe(take(1), map(user => {
+    if(user){
+      return true
+    }else{
+      router.navigate(['/'])
+      return false
+    }
+  }))
 };
